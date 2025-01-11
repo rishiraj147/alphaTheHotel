43840414c0b659e0e31688ac81335106150feb23
Rishi Kumar <kumarrishi@Rishis-MacBook-Air.local>
2025-01-12 01:58:26 +0530

    database configuration has been added

---
+++ backend/src/main/java/com/alphadev/AlphaHotel/AlphaHotelApplication.java
@@ -0,0 +1,13 @@
+package com.alphadev.AlphaHotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphaHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlphaHotelApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/alphadev/AlphaHotel/AlphaHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alphadev.AlphaHotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlphaHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
